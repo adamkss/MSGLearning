c1e5f02a3c010177cc124ab1d2520634d719d720
Kiss Adam <kiss.adam.a@gmail.com>
2018-04-18 10:26:58 +0300

    Add Example 6 with Embeddable

---
+++ src/main/java/JPALearning/Example6/MainClass.java
@@ -0,0 +1,35 @@
+package JPALearning.Example6;
+
+import JPALearning.Example5.Models.Product;
+import JPALearning.Example5.Models.ShoppingCart;
+import JPALearning.Example5.Models.ShoppingCartItem;
+import JPALearning.Example6.Models.Address;
+import JPALearning.Example6.Models.Employee;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class MainClass {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("org.hibernate.tutorial.jpa");
+
+        Address address1 = new Address();
+        address1.setCity("Salonta");
+        address1.setState("Romania");
+        address1.setZip("415500");
+        address1.setStreet("Jokai mor");
+
+        Employee employee1 = new Employee();
+        employee1.setAddress(address1);
+        employee1.setName("Robi");
+        employee1.setSalary(1222);
+
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(employee1);
+        entityManager.getTransaction().commit();
+        entityManagerFactory.close();
+    }
+}
